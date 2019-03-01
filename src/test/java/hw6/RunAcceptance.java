cb6b275ddf14ce3176a78fd6c0562113277ba8e0
AnastasiyaFeofanova <feoanastasiya@gmail.com>
2019-03-02 01:06:21 +0300

    Runner for HW6

--- src/test/java/hw6/RunAcceptance.java
+++ src/test/java/hw6/RunAcceptance.java
@@ -2,8 +2,14 @@ package hw6;
 
 import com.codeborne.selenide.Configuration;
 import cucumber.api.CucumberOptions;
+import cucumber.api.java.After;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
+
+import static com.codeborne.selenide.Selenide.close;
 
 @CucumberOptions(
         features = "classpath:hw6",
@@ -11,9 +17,13 @@ import org.testng.annotations.BeforeSuite;
 )
 public class RunAcceptance extends AbstractTestNGCucumberTests {
     @BeforeSuite
-    public void driverSetup(){
+        public void driverSetup(){
         Configuration.browser = "Chrome";
         Configuration.timeout = 5000;
         Configuration.pollingInterval = 300;
+    }
+    @After
+    public void after(){
+        close();
     }
 }
